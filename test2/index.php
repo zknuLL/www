32ba916829688ade2b225cb1f52c8c80c38b9923
jccjd <1340944538@qq.com>
2017-09-28 22:55:07 +0800

    update

---
+++ test2/index.php
@@ -0,0 +1,25 @@
+<?php
+	$_COOKIE['name'] = "user1";
+
+	echo "<pre>";
+	print_r($_COOKIE);
+	echo "</pre>";
+
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<title></title>
+ </head>
+
+ <body>
+	 <p><h1>hello word</h1></p>
+	 <p><h2>hello word</h2></p>
+	 <p><h1>hello word</h1></p>
+	 <p><h1>hello word</h1></p>
+	 <p><h1>hello word</h1></p>
+	 <p><h1>sds</h1></p>
+	 <script type="text/javascript">
+ </body>
+ </html>
